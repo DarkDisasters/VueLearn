fa6c2543582546e2ad2960d37181fabf1cdd8ab7
DarkDisaters <2361629815@qq.com>
2019-08-28 10:38:22 +0800

    vue-webpack-router

---
+++ webpack-vue-router/src/subcom/login.vue
@@ -0,0 +1,19 @@
+<template>
+    <div>
+        <h3>这是account的登录子组件</h3>
+    </div>
+</template>
+
+<script>
+    export default {
+        
+    }
+</script>
+
+<style scoped>
+    /* 样式的 scoped 是通过css的属性选择器实现的 */
+    div{
+        color:blueviolet;
+    }
+</style>
+
---
+++ webpack-vue-router/src/subcom/register.vue
@@ -0,0 +1,18 @@
+<template>
+    <h3>这是account的注册子组件</h3>
+</template>
+
+<script>
+    export default {
+        
+    }
+</script>
+
+<style lang="scss" scoped>
+    /* 普通的 style 标签只支持普通的样式，如果想要使用 scss 或 less，需要为style元素设置一个lang属性 */
+    body{
+        div{
+            font-style: italic;
+        }
+    }
+</style>
