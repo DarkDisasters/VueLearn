abd897dd65ba93a6db26ef3986addb1b8136445a
王帅琦 <wangshuaiqi04@meituan.com>
2020-06-09 21:34:00 +0800

    basic knowledge learn 60%

---
+++ 文档上学习/vuex/test.js
@@ -0,0 +1,13 @@
+var a = 1
+
+if (a == 5) {
+    console.log('aaaa')
+} else if (a == 2) {
+    console.log('bbbb')
+} else {
+    if (a == 0) {
+        console.log('cccc')
+    }
+} else {
+    console.log('dddd')
+}
